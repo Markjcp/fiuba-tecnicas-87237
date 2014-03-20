27f3f65bc5038704d48c8e906143037128ff0616
Marcos <marcos@marcos-laptop.(none)>
2014-03-20 01:17:51 -0300

    Initial commit

---
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Paper.java
@@ -0,0 +1,17 @@
+package ar.fiuba.tecnicas.rockpaperscissors;
+
+public class Paper {
+
+	public Scissors vs(Scissors scissors) {
+		return scissors;
+	}
+
+	public Paper vs(Rock rock) {
+		return this;
+	}
+
+	public Paper vs(Paper paper) {
+		return this;
+	}
+
+}
---
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
@@ -0,0 +1,17 @@
+package ar.fiuba.tecnicas.rockpaperscissors;
+
+public class Scissors {
+
+	public Scissors vs(Scissors scissors) {
+		return this;
+	}
+
+	public Scissors vs(Paper paper) {
+		return this;
+	}
+
+	public Rock vs(Rock rock) {
+		return rock;
+	}
+
+}
